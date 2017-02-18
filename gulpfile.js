173a1f680c21bf591a50e6e16300d2a642889c2b
Masoud Mirzaei <mamos98@gmail.com>
2017-02-18 19:41:59 +0330

    Add default task to gulp file 🤷

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
-var gulp = require('gulp'),
-  babel = require('babelify'),
-  browserify = require('browserify'),
-  buffer = require('vinyl-buffer'),
-  source = require('vinyl-source-stream'),
-  sourcemaps = require('gulp-sourcemaps');
+var gulp      = require('gulp'),
+  babel       = require('babelify'),
+  browserify  = require('browserify'),
+  buffer      = require('vinyl-buffer'),
+  source      = require('vinyl-source-stream'),
+  sourcemaps  = require('gulp-sourcemaps');
 
-gulp.task('scripts.bundle', function () {
-  return browserify("./source/index.js")
-    .transform(babel)
-    .bundle()
-    .pipe(source('keensole.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./dist'));
-});
+gulp
+  // Transform ES6 Modules to ES5 and Bundle <keensole 👨‍💻> 
+  .task('scripts.bundle', function () {
+    return browserify("./source/index.js")
+      .transform(babel)
+      .bundle()
+      .pipe(source('keensole.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init())
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./dist'));
+  })
 
-gulp.task('build', ['scripts.bundle']);
+  .task('build', ['scripts.bundle'])
+
+  .task('deafult', ['build']);
